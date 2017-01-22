8b7f6be6116232fdb7925bde045ce3fabb370d0c
Admin <hola@cometorta.com.ar>
2017-01-22 11:43:51 -0300

    v.0.1

---
+++ cometorta/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cometorta project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cometorta.settings")
+
+application = get_wsgi_application()
